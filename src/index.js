a8dcea3063bc3c69e81ccd98d13707506e2c71ea
Kateryna Gordiienko <gordkat@gmail.com>
2021-03-24 07:26:26 +0100

    finished async

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import '@pnotify/core/dist/BrightTheme.css';
 import { error } from '@pnotify/core';
 import apiService from './js/apiService';
 import cardTpl from './templates/img-card.hbs';
-import lodash from 'lodash';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/src/styles/main.scss';
 
@@ -31,26 +30,24 @@ const renderCards = cards => {
   refs.gallery.insertAdjacentHTML('beforeend', cardsMarkup);
 };
 
-const onSearch = event => {
+const onSearch = async event => {
   event.preventDefault();
   clearPage();
-  apiService.searchQuery = event.target.elements.query.value;
+  apiService.searchQuery = event.target.elements.query.value.trim();
   if (!apiService.searchQuery) {
     notice('Введите что-то!');
     return;
   }
-  apiService
-    .fetchByName()
-    .then(result => {
-      if (result.hits.length === 0) {
-        throw new Error();
-      }
-      apiService.resetPage();
-      renderCards(result.hits);
-    })
-    .catch(err => {
-      notice('По вашему запросу картинок не найдено. Попробуйте еще раз!');
-    });
+  try {
+    const result = await apiService.fetchByName();
+    if (result.hits.length === 0) {
+      throw new Error();
+    }
+    apiService.resetPage();
+    renderCards(result.hits);
+  } catch {
+    notice('По вашему запросу картинок не найдено. Попробуйте еще раз!');
+  }
 };
 
 refs.form.addEventListener('submit', onSearch);
@@ -67,28 +64,26 @@ const onModal = event => {
 
 refs.gallery.addEventListener('click', onModal);
 
-const onObserver = lodash.debounce(entries => {
-  entries.forEach(entry => {
+const onObserver = entries => {
+  entries.forEach(async entry => {
     if (
       entry.isIntersecting &&
       apiService.searchQuery &&
       refs.gallery.textContent
     ) {
       apiService.nextPage();
-      apiService
-        .fetchByName()
-        .then(result => {
-          if (result.hits.length === 0) {
-            throw new Error();
-          }
-          renderCards(result.hits);
-        })
-        .catch(err => {
-          notice('Картинок по этому запросу больше нет. Делайте новый запрос!');
-        });
+      try {
+        const result = await apiService.fetchByName();
+        if (result.hits.length === 0) {
+          throw new Error();
+        }
+        renderCards(result.hits);
+      } catch {
+        notice('Картинок по этому запросу больше нет. Делайте новый запрос!');
+      }
     }
   });
-}, 300);
+};
 const options = {
   rootMargin: '150px',
 };
